1274653274c6e8e004e601eeec9c6fd8042ebfef
Memedsundu <146135762+Memedsundu@users.noreply.github.com>
2024-11-04 14:07:41 +0100

    Create create_database.sql

---
+++ create_database.sql
@@ -0,0 +1,6 @@
+-- create_database.sql
+-- This script creates the LanguageEndangermentDB database.
+
+DROP DATABASE IF EXISTS LanguageEndangermentDB;
+CREATE DATABASE LanguageEndangermentDB;
+USE LanguageEndangermentDB;
